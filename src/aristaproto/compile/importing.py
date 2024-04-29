danielgtaylor/python-betterproto

fix: support pydantic-version google pb (#568)
* feat: pydantic version of google pb

* fix: patch pb Struct to support json, dict rountrip

* fix: pydantic-version google pb, json, dict rntrip

* chore: remove `@generated`, remove gen, code fmt

* chore: test case for pydantic-version google pb

src/betterproto/compile/importing.py
@@ -43,7 +43,12 @@ def parse_source_type_name(field_type_name: str) -> Tuple[str, str]:
 
 
 def get_type_reference(
-    *, package: str, imports: set, source_type: str, unwrap: bool = True
+    *,
+    package: str,
+    imports: set,
+    source_type: str,
+    unwrap: bool = True,
+    pydantic: bool = False,
 ) -> str:
     """
     Return a Python type name for a proto type reference. Adds the import if
@@ -69,7 +74,9 @@ def get_type_reference(
     compiling_google_protobuf = current_package == ["google", "protobuf"]
     importing_google_protobuf = py_package == ["google", "protobuf"]
     if importing_google_protobuf and not compiling_google_protobuf:
-        py_package = ["betterproto", "lib"] + py_package
+        py_package = (
+            ["betterproto", "lib"] + (["pydantic"] if pydantic else []) + py_package
+        )
 
     if py_package[:1] == ["betterproto"]:
         return reference_absolute(imports, py_package, py_type)

tests/test_struct.py
@@ -1,6 +1,7 @@
 import json
 
 from betterproto.lib.google.protobuf import Struct
+from betterproto.lib.pydantic.google.protobuf import Struct as StructPydantic
 
 
 def test_struct_roundtrip():
@@ -22,3 +23,14 @@ def test_struct_roundtrip():
     assert struct_from_json.to_dict() == data
     assert struct_from_json == struct_from_dict
     assert struct_from_json.to_json() == data_json
+
+    struct_pyd_from_dict = StructPydantic(fields={}).from_dict(data)
+    assert struct_pyd_from_dict.fields == data
+    assert struct_pyd_from_dict.to_dict() == data
+    assert struct_pyd_from_dict.to_json() == data_json
+
+    struct_pyd_from_dict = StructPydantic(fields={}).from_json(data_json)
+    assert struct_pyd_from_dict.fields == data
+    assert struct_pyd_from_dict.to_dict() == data
+    assert struct_pyd_from_dict == struct_pyd_from_dict
+    assert struct_pyd_from_dict.to_json() == data_json

aristanetworks/python-aristaproto

src/aristaproto/compile/importing.py
@@ -43,7 +43,12 @@ def parse_source_type_name(field_type_name: str) -> Tuple[str, str]:
 
 
 def get_type_reference(
-    *, package: str, imports: set, source_type: str, unwrap: bool = True
+    *,
+    package: str,
+    imports: set,
+    source_type: str,
+    unwrap: bool = True,
+    pydantic: bool = False,
 ) -> str:
     """
     Return a Python type name for a proto type reference. Adds the import if
@@ -69,7 +74,9 @@ def get_type_reference(
     compiling_google_protobuf = current_package == ["google", "protobuf"]
     importing_google_protobuf = py_package == ["google", "protobuf"]
     if importing_google_protobuf and not compiling_google_protobuf:
-        py_package = ["aristaproto", "lib"] + py_package
+        py_package = (
+            ["aristaproto", "lib"] + (["pydantic"] if pydantic else []) + py_package
+        )
 
     if py_package[:1] == ["aristaproto"]:
         return reference_absolute(imports, py_package, py_type)

tests/test_struct.py
@@ -1,6 +1,7 @@
 import json
 
 from aristaproto.lib.google.protobuf import Struct
+from aristaproto.lib.pydantic.google.protobuf import Struct as StructPydantic
 
 
 def test_struct_roundtrip():
@@ -22,3 +23,14 @@ def test_struct_roundtrip():
     assert struct_from_json.to_dict() == data
     assert struct_from_json == struct_from_dict
     assert struct_from_json.to_json() == data_json
+
+    struct_pyd_from_dict = StructPydantic(fields={}).from_dict(data)
+    assert struct_pyd_from_dict.fields == data
+    assert struct_pyd_from_dict.to_dict() == data
+    assert struct_pyd_from_dict.to_json() == data_json
+
+    struct_pyd_from_dict = StructPydantic(fields={}).from_json(data_json)
+    assert struct_pyd_from_dict.fields == data
+    assert struct_pyd_from_dict.to_dict() == data
+    assert struct_pyd_from_dict == struct_pyd_from_dict
+    assert struct_pyd_from_dict.to_json() == data_json