danielgtaylor/python-betterproto

Prevent users from creating messages with wrong parameters when pydantic is used (#615)

tests/inputs/invalid_field/test_invalid_field.py
@@ -1,0 +1,17 @@
+import pytest
+
+
+def test_invalid_field():
+    from tests.output_betterproto.invalid_field import Test
+
+    with pytest.raises(TypeError):
+        Test(unknown_field=12)
+
+
+def test_invalid_field_pydantic():
+    from pydantic import ValidationError
+
+    from tests.output_betterproto_pydantic.invalid_field import Test
+
+    with pytest.raises(ValidationError):
+        Test(unknown_field=12)

aristanetworks/python-aristaproto

tests/inputs/invalid_field/test_invalid_field.py
@@ -1,0 +1,17 @@
+import pytest
+
+
+def test_invalid_field():
+    from tests.output_aristaprotoproto.invalid_field import Test
+
+    with pytest.raises(TypeError):
+        Test(unknown_field=12)
+
+
+def test_invalid_field_pydantic():
+    from pydantic import ValidationError
+
+    from tests.output_aristaprotoproto_pydantic.invalid_field import Test
+
+    with pytest.raises(ValidationError):
+        Test(unknown_field=12)