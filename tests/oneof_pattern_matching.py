ebcdec4d0fb6538219f7e2809327f44d462fb219
Claus Holbech <holbech@arista.com>
2026-01-12 10:41:36 +0100

    Refactor: Update Ruff in pre-commit and update test to be Py39 compatible (#85)

--- tests/oneof_pattern_matching.py
+++ tests/oneof_pattern_matching.py
@@ -19,28 +19,59 @@ def test_oneof_pattern_matching():
 
     foo = Foo(baz="test1", abc="test2")
 
-    match foo:
-        case Foo(bar=_):
-            pytest.fail("Matched 'bar' instead of 'baz'")
-        case Foo(baz=v):
-            assert v == "test1"
-        case _:
-            pytest.fail("Matched neither 'bar' nor 'baz'")
+    # Test group1: Check if we can access baz (should be set) vs bar (should not be accessible)
+    # Pattern matching would check if foo matches Foo(bar=_) first, then Foo(baz=v)
+    # Since bar is not the active oneof field, accessing it raises AttributeError
+    # So we check if baz is accessible and has the right value
+    try:
+        # Try to access bar - should raise AttributeError since baz is set
+        _ = foo.bar
+        pytest.fail("Matched 'bar' instead of 'baz'")
+    except AttributeError:
+        # Expected - bar is not set in the oneof group
+        pass
 
-    match foo:
-        case Foo(sub=_):
-            pytest.fail("Matched 'sub' instead of 'abc'")
-        case Foo(abc=v):
-            assert v == "test2"
-        case _:
-            pytest.fail("Matched neither 'sub' nor 'abc'")
+    # Now check baz is accessible and has correct value
+    try:
+        v = foo.baz
+        assert v == "test1"
+    except AttributeError:
+        pytest.fail("Matched neither 'bar' nor 'baz'")
 
+    # Test group2: Check if we can access abc (should be set) vs sub (should not be accessible)
+    try:
+        # Try to access sub - should raise AttributeError since abc is set
+        _ = foo.sub
+        pytest.fail("Matched 'sub' instead of 'abc'")
+    except AttributeError:
+        # Expected - sub is not set in the oneof group
+        pass
+
+    # Now check abc is accessible and has correct value
+    try:
+        v = foo.abc
+        assert v == "test2"
+    except AttributeError:
+        pytest.fail("Matched neither 'sub' nor 'abc'")
+
+    # Set sub, which should make it the active field and abc inaccessible
     foo.sub = Sub(val=1)
 
-    match foo:
-        case Foo(sub=Sub(val=v)):
+    # Test group2 again: Now sub should be accessible and abc should not be
+    try:
+        # Check if sub is set and has the right nested value
+        if isinstance(foo.sub, Sub) and foo.sub.val == 1:
+            v = foo.sub.val
             assert v == 1
-        case Foo(abc=v):
-            pytest.fail("Matched 'abc' instead of 'sub'")
-        case _:
+        else:
             pytest.fail("Matched neither 'sub' nor 'abc'")
+    except AttributeError:
+        pytest.fail("Matched neither 'sub' nor 'abc'")
+
+    # Now abc should raise AttributeError
+    try:
+        _ = foo.abc
+        pytest.fail("Matched 'abc' instead of 'sub'")
+    except AttributeError:
+        # Expected - abc is no longer the active field in group2
+        pass
